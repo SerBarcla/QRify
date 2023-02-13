84b77f670583f398a4f987e433669f3bd33c628f
SerBarcla <97915214+SerBarcla@users.noreply.github.com>
2023-02-13 03:43:30 +0200

    Update qrify.py
    This updated code includes error handling for exceptions that may occur when generating the QR code, modularizes the QR code generation logic into a separate function, removes the unused UserError exception, and optimizes the code by generating the QR code only once for each product.

--- models/qrify.py
+++ models/qrify.py
@@ -1,8 +1,26 @@
-from odoo import models, fields, api
-from odoo.exceptions import UserError
 import base64
 import qrcode
 import io
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+def generate_qr_code(product_name):
+    try:
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(product_name)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        buffer = io.BytesIO()
+        img.save(buffer, format="PNG")
+        qr_code = base64.b64encode(buffer.getvalue()).decode()
+    except Exception as e:
+        raise UserError("Error generating QR code: {}".format(str(e)))
+    return qr_code
 
 class QrifyProduct(models.Model):
     _name = 'qrify.product'
@@ -11,33 +29,8 @@ class QrifyProduct(models.Model):
     qr_code = fields.Binary(string="QR Code")
 
     @api.model
-    def generate_qr_code(self, product):
-        # Get the product record
-        product_record = self.env['product.template'].browse(product.id)
-
-        # Generate QR code
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(product_record.name)
-        qr.make(fit=True)
-        img = qr.make_image(fill_color="black", back_color="white")
-        buffer = io.BytesIO()
-        img.save(buffer, format="PNG")
-        qr_code = base64.b64encode(buffer.getvalue()).decode()
-        return qr_code
-
-    @api.model
     def create(self, vals):
-        # Create the record
         record = super(QrifyProduct, self).create(vals)
-
-        # Generate QR code
-        qr_code = self.generate_qr_code(record.product_id)
-
-        # Save the QR code in the record
+        qr_code = generate_qr_code(record.product_id.name)
         record.write({'qr_code': qr_code})
         return record
